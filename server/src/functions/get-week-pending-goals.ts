27479e1a8cfc1efc939d5605b1df126759725595
Geovanni Magnani <magnanigeovanni@gmail.com>
2024-09-10 17:31:17 -0300

    Finalizado rotas iniciais

--- server/src/functions/get-week-pending-goals.ts
+++ server/src/functions/get-week-pending-goals.ts
@@ -1,3 +1,46 @@
-export function getWeekPendingGoals(){
-    
+import dayjs from "dayjs";
+import  weekOfYear  from "dayjs/plugin/weekOfYear";
+import { count, gte, lte, eq, sql } from "drizzle-orm";
+import { goalCompletions, goals } from "../db/schema";
+import { db } from "../db";
+
+
+export async function getWeekPendingGoals(){
+    const lastDayOfWeek = dayjs().endOf('week').toDate()
+    const firstDayOfWeek = dayjs().startOf('week').toDate()
+
+    const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week')as(
+        db.select(
+            {
+                id: goals.id,
+                title: goals.title,
+                desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+                createdAt: goals.createdAt, 
+            }
+        ).from(goals).where(lte(goals.createdAt, lastDayOfWeek))
+    )
+   
+    const goalCompletionCounts = db.$with('goal_completion_counts')as(
+        db.select({
+            goalId: goalCompletions.goalId,
+            completionCount: count(goalCompletions.id).as('completionCount'),
+        }).from(goalCompletions).where(gte(goalCompletions.createdAt, firstDayOfWeek),lte(goalCompletions.createdAt, lastDayOfWeek)).groupBy(goalCompletions.goalId)
+    )
+
+
+    const pendingGoals = await db.with(goalsCreatedUpToWeek, goalCompletionCounts)
+    .select({
+        id: goalsCreatedUpToWeek.id,
+        title: goalsCreatedUpToWeek.title,
+        desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+        completionCount: sql`
+            COALESCE(${goalCompletionCounts.completionCount}, 0)
+        `.mapWith(Number)
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId,goalsCreatedUpToWeek.goalId ))
+
+    return {
+        pendingGoals,
+    }
 }
