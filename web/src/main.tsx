e64542d7cd0f505c4c3d9571c7eddc5a16c6f6f7
Geovanni Magnani <magnanigeovanni@gmail.com>
2024-09-12 18:05:43 -0300

    Inicio da ligação do front com o back

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {App} from './app'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-
+const querryClient = new QueryClient()
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
+  
+  <QueryClientProvider client={querryClient}>
   <StrictMode>
     <App />
   </StrictMode>,
+
+  </QueryClientProvider>
 )
